39e7d0fd062e6b8a7beadd3113873fdec90c0e32
ianwijma <ian@wij.ma>
2020-12-08 10:45:38 +1100

    Added some retries and logging

--- src/link/repositories/link.repository.ts
+++ src/link/repositories/link.repository.ts
@@ -2,11 +2,13 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import { LinkEntity } from '../entities/link.entity';
 import { NewLinkDto } from '../dto/new-link.dto';
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { nanoid } from 'nanoid';
 
 @EntityRepository(LinkEntity)
 export class LinkRepository extends Repository<LinkEntity> {
+  private logger: Logger = new Logger('LinkRepository');
+
   getOneByLink(link: string) {
     return this.findOne({ link });
   }
@@ -15,19 +17,27 @@ export class LinkRepository extends Repository<LinkEntity> {
     return this.findOne({ shortid });
   }
 
-  async createLink(newLinkDto: NewLinkDto) {
+  async createLink(newLinkDto: NewLinkDto, retry = 0, shortidSize = 5) {
     const { link } = newLinkDto;
+
+    if (retry >= 5) {
+      this.logger.log(
+        `Tried to create a line for 5 times, enlarging the shortid size to ${shortidSize}`,
+      );
+      shortidSize++;
+    }
 
     const linkObj = new LinkEntity();
     linkObj.link = link;
-    linkObj.shortid = await nanoid();
+    linkObj.shortid = await nanoid(8);
     try {
       await linkObj.save();
     } catch (error) {
       const { code } = error;
       if (code === '23505') {
         // retry
-        return await this.createLink(newLinkDto);
+        this.logger.log(`Link with shortid ${linkObj.shortid} already exists`);
+        return await this.createLink(newLinkDto, ++retry, shortidSize);
       } else {
         throw new InternalServerErrorException();
       }
